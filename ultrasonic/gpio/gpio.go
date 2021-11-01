4539cd2f4fdaac47d55d09d07a736f11453a3688
assaf <asssaf@localhost>
2021-11-01 16:00:48 -0700

    make sure the trigger is initally low before doing the trigger pulse

--- ultrasonic/gpio/gpio.go
+++ ultrasonic/gpio/gpio.go
@@ -38,7 +38,15 @@ func (d *dev) Sense(values *ultrasonic.SensorValues) error {
 }
 
 func (d *dev) senseDistance() (physic.Distance, error) {
-	err := d.triggerPin.Out(gpio.High)
+	// make sure output is low
+	err := d.triggerPin.Out(gpio.Low)
+	if err != nil {
+		return 0, err
+	}
+	time.Sleep(60 * time.Millisecond)
+
+	// create the trigger pulse
+	err = d.triggerPin.Out(gpio.High)
 	if err != nil {
 		return 0, err
 	}
